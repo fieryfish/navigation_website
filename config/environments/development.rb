819f8450b2131e9589ff6bdf99f9ab952b402035
yulong <yulong@rd.tuan800.com>
2013-12-16 21:48:23 +0000

    finish email confirmation,then mange user page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,24 @@ Nav::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
   # devise recommend
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # test devise email step:
+  # 1 mailcatcher
+  # 2 Go to http://localhost:1080/
+  # 3 Send mail through smtp://localhost:1025
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "yulong.lewei",
+    :password => "ZAQ12WSX"
+  }
 end
