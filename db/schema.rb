3bbc5cc647072cdf5cf22b86a3e1133d11eeef57
yulong <yulong@rd.tuan800.com>
2013-12-17 15:52:44 +0000

    you can login in through both name and email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216165153) do
+ActiveRecord::Schema.define(version: 20131217151236) do
 
   create_table "link_tag_sites", force: true do |t|
     t.integer  "tag_id",     default: 0, null: false
@@ -61,10 +61,12 @@ ActiveRecord::Schema.define(version: 20131216165153) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
