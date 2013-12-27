b5272f331c8c766dd8678dc06c7c3baf2f7d7041
yulong <yulong@rd.tuan800.com>
2013-12-27 20:12:42 +0000

    devise update without psd

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,55 @@
+class RegistrationsController < Devise::RegistrationsController 
+  #def update
+    ## For Rails 4
+    #account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
+    ## For Rails 3
+    ## account_update_params = params[:user]
+
+    ## required for settings form to submit when password is left blank
+    #if account_update_params[:password].blank?
+      #account_update_params.delete("password")
+      #account_update_params.delete("password_confirmation")
+    #end
+
+    #@user = User.find(current_user.id)
+    #if @user.update_attributes(account_update_params)
+      #set_flash_message :notice, :updated
+      ## Sign in the user bypassing validation in case his password changed
+      #sign_in @user, :bypass => true
+      #redirect_to after_update_path_for(@user)
+    #else
+      #render "edit"
+    #end
+  #end
+
+  def update
+    @user = User.find(current_user.id)
+    successfully_updated = if needs_password?(@user, params)
+                             @user.update_with_password(devise_parameter_sanitizer.sanitize(:account_update))
+                           else
+                             # remove the virtual current_password attribute update_without_password
+                             # doesn't know how to ignore it
+                             params[:user].delete(:current_password)
+                             @user.update_without_password(devise_parameter_sanitizer.sanitize(:account_update))
+                           end
+
+    if successfully_updated
+      set_flash_message :notice, :updated
+      # Sign in the user bypassing validation in case his password changed
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  # check if we need password to update user data
+  # ie if password or email was changed
+  # extend this as needed
+  def needs_password?(user, params)
+      params[:user][:password].present?
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Nav::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'main#index'
-
-   get 'main/index'
-   #get 'search_suggestion/index'
-   resources :search_suggestion do
-   end
+  root 'main#index'
+  get 'main/index'
+  #get 'search_suggestion/index'
+  resources :search_suggestion do
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
